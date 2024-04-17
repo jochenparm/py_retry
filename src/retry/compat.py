af11d7da4c016b7e7fbdd5e7b47f8b295b27eaac
Jochen Parmentier <jochen.parmentier@external.atlascopco.com>
2024-04-17 18:19:37 +0200

    Adding docstrings to the compat.py module

--- src/retry/compat.py
+++ src/retry/compat.py
@@ -21,5 +21,17 @@ def decorator(caller: Callable):
 
 
 class NullHandler(logging.Handler):
+    """
+    A custom logging handler that does nothing when emitting log records.
+
+    This class represents a logging handler that inherits from `logging.Handler`.
+    The `emit` method is overridden to perform no action, effectively discarding log records.
+    """
+
     def emit(self, record: logging.LogRecord) -> None:
+        """
+        This method simply discards the provied log record and does nothing.
+
+        :returns: None
+        """
         pass
