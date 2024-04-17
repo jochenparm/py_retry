55006364f33ff88d19e7b35a96dd0492cf276deb
Jochen Parmentier <jochen.parmentier@external.atlascopco.com>
2024-04-17 16:07:48 +0200

    Simplify the __retry_internal and retry functions

--- src/retry/api.py
+++ src/retry/api.py
@@ -1,8 +1,7 @@
 import logging
 import random
 import time
-from functools import partial
-from typing import Any, Callable, NewType, Optional, TypeVar, Union
+from typing import Any, Callable, NewType, Optional, TypedDict, TypeVar, Union
 
 from .compat import decorator
 
@@ -10,19 +9,29 @@ logging_logger = logging.getLogger(__name__)
 formatter = "%(asctime)s - %(name)s - L%(lineno)d - %(levelname)s - %(message)s"
 logging.basicConfig(format=formatter)
 
+
 T_Exception = NewType("T_Exception", Exception)
 T_I_F = TypeVar("T_I_F", bound=Union[int, float])
 
 
+class RetryConfig(TypedDict):
+    exceptions: Union[T_Exception, tuple[T_Exception, ...]] = Exception
+    tries: int = -1
+    delay: int = 0
+    max_delay: Optional[int] = None
+    backoff: int = 1
+    jitter: Union[T_I_F, tuple[T_I_F, ...]] = 0
+    logger: Optional[logging.Logger] = logging_logger
+
+
+T_RetryConfig = NewType("T_RetryConfig", RetryConfig)
+
+
 def __retry_internal(
     f: Callable,
-    exceptions: Union[T_Exception, tuple[T_Exception, ...]] = Exception,
-    tries: int = -1,
-    delay: int = 0,
-    max_delay: Optional[int] = None,
-    backoff: int = 1,
-    jitter: Union[T_I_F, tuple[T_I_F, ...]] = 0,
-    logger: Optional[logging.Logger] = logging_logger,
+    config: T_RetryConfig,
+    f_args: Optional[Any] = None,
+    f_kwargs: Optional[Any] = None,
 ) -> Optional[Callable]:
     """
     Executes a function and retries it if it failed.
@@ -39,25 +48,31 @@ def __retry_internal(
                     default: retry.logging_logger. if None, logging is disabled.
     :returns: the result of the f function.
     """
-    _tries, _delay = tries, delay
+    _delay = config["delay"]
+    _tries = config["tries"]
 
-    while _tries:
+    while _tries > 0:
         try:
-            return f()
-        except exceptions as e:
-            _tries -= 1
-            if not _tries:
+            return f(*f_args, **f_kwargs)
+        except config["exceptions"] as e:
+            _tries -= 1 if _tries > 0 else 0
+            if _tries == 0:
                 raise
 
-            if logger:
-                logger.warning(f"{e}, retrying in {_delay} seconds...")
+            if config["logger"]:
+                config["logger"].warning(f"{e}, retrying in {_delay} seconds...")
 
             time.sleep(_delay)
-            _delay *= backoff
-
-            _delay += random.uniform(*jitter) if isinstance(jitter, tuple) else jitter
-            if max_delay:
-                _delay = min(_delay, max_delay)
+            jitter_value = (
+                random.uniform(*config["jitter"])
+                if isinstance(config["jitter"], tuple)
+                else config["jitter"]
+            )
+            _delay = (
+                min(_delay * config["backoff"] + jitter_value, config["max_delay"])
+                if config["max_delay"]
+                else _delay * config["backoff"] + jitter_value
+            )
 
 
 def retry(
@@ -82,21 +97,22 @@ def retry(
                     default: retry.logging_logger. if None, logging is disabled.
     :returns: a retry decorator.
     """
+    config: RetryConfig = {
+        "exceptions": exceptions,
+        "tries": tries,
+        "delay": delay,
+        "max_delay": max_delay,
+        "backoff": backoff,
+        "jitter": (jitter, jitter) if isinstance(jitter, (int, float)) else jitter,
+        "logger": logger,
+    }
 
-    @decorator
-    def retry_decorator(f: Callable, *f_args, **f_kwargs):
-        args = f_args if f_args else []
-        kwargs = f_kwargs if f_kwargs else {}
-        return __retry_internal(
-            partial(f, *args, **kwargs),
-            exceptions,
-            tries,
-            delay,
-            max_delay,
-            backoff,
-            jitter,
-            logger,
-        )
+    def retry_decorator(f: Callable):
+        @decorator
+        def wrapper(*f_args, **f_kwargs):
+            return __retry_internal(f, config, f_args, f_kwargs)
+
+        return wrapper
 
     return retry_decorator
 
@@ -130,15 +146,13 @@ def retry_call(
                     default: retry.logging_logger. if None, logging is disabled.
     :returns: the result of the f function.
     """
-    args = f_args if f_args else []
-    kwargs = f_kwargs if f_kwargs else {}
-    return __retry_internal(
-        partial(f, *args, **kwargs),
-        exceptions,
-        tries,
-        delay,
-        max_delay,
-        backoff,
-        jitter,
-        logger,
-    )
+    config: RetryConfig = {
+        "exceptions": exceptions,
+        "tries": tries,
+        "delay": delay,
+        "max_delay": max_delay,
+        "backoff": backoff,
+        "jitter": (jitter, jitter) if isinstance(jitter, (int, float)) else jitter,
+        "logger": logger,
+    }
+    return __retry_internal(f, config, f_args, f_kwargs)
